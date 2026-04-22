5425ca79a2a76b00b2b3963d6d1386c45baaf028
pengdake <19921207pq@gmail.com>
2026-04-22 15:53:47 +0800

    update logger format

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,8 +1,21 @@
-from loguru import logger
 import sys
+import os
+from loguru import logger
+
 
 def setup_logger():
-    logger.remove()  # Remove default logger
-    format = "{time} - {level} - {name} - {function} - {line} - {message} - {extra}"
-    logger.add(sys.stdout, level="INFO", format=format, serialize=True, backtrace=True, diagnose=True)
+    logger.remove()
+
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    env = os.getenv("ENV", "prod")
+
+    logger.add(
+        sys.stdout,
+        level=log_level,
+        serialize=True,
+        enqueue=True,
+        backtrace=env == "dev",
+        diagnose=env == "dev",
+    )
+
     return logger
--- app/utils/acoustid.py
+++ app/utils/acoustid.py
@@ -149,6 +149,8 @@ def sync_music_metadata(music_dir):
     return failed_files
 
 if __name__ == "__main__":
+    from app.core.logger import setup_logger
+    setup_logger()
     music_dir = sys.argv[1]
     if not os.path.isdir(music_dir):
         logger.error(f"Provided path is not a directory: {music_dir}")
